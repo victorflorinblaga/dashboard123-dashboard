3803dae385c4b35cf78c10885deb5e1cd0931105
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2025-01-10 10:44:34 +0100

    updating contents of template/src/app/components/panel3.tsx

--- template/src/app/components/panel3.tsx
+++ template/src/app/components/panel3.tsx
@@ -1,12 +1,46 @@
+"use client";
 
-function panel3() {
-    return (
-      <div>
-        <h1>Panel 3</h1>
-      </div>
-    );
-  }
-  
-  export default panel3;
-  
-  
+import React from "react";
+import { useEffect, useState } from "react";
+import { useQuery } from "@/hooks/useQuery";
+import LoadingIndicator from "@/components/LoadingIndicator";
+import { query } from "@/queries/generated/jandavid.stuetz@gmail.com/1736429965000/query";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Page() {
+  const [headers, rows, loading] = useQuery(query);
+
+  if (loading) return <LoadingIndicator />;
+
+  return (
+    <div className='w-full h-full'>
+      <Table>
+        <TableCaption>A list of data entries.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableHead key={index}>{header}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <TableCell key={cellIndex}>{cell}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
